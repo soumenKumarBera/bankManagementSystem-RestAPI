9f13c43d53f14e6b4aed7b5f4418e2d69e1acfdf
soumen kumar bera <soumenbera4017@gmail.com>
2026-03-10 22:43:31 +0530

    Create springboot Project

---
+++ src/main/java/in/kb/main/BankManagementSystemRestApiApplication.java
@@ -0,0 +1,13 @@
+package in.kb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSystemRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSystemRestApiApplication.class, args);
+	}
+
+}
